b964bf0ae03a7f11d7b80ea2b2e9b25d0dfc8c54
Akos <barcza.akos15@gmail.com>
2022-09-08 22:18:49 +0200

    rename user to copyFiles

---
+++ frontend/copyFiles/script.js
@@ -0,0 +1,74 @@
+let rootElement;
+let artist;
+let title;
+
+async function initRender() {
+    rootElement = document.querySelector('#root');
+    rootElement.addEventListener('click', globalSubmitHandler);
+    await getGenres();
+    artist = document.querySelector('#artist');
+    title = document.querySelector('#title');
+}
+
+//
+
+function globalSubmitHandler(event) {
+    if (event.target.id === 'submit') {
+        console.log('click')
+    }
+}
+
+/* function getInputValues() {
+    console.log(artist.value)
+    console.log(title.value)
+    console.log(file.value)
+}
+
+function clickSubmit(e) {
+    e.preventDefault();
+    if (e.target.id === 'submit') {
+        getInputValues()
+    }
+} */
+
+async function getGenres() {
+    let url = `/api/genre`;
+    let response = await fetch(url);
+    let result = await response.json();
+    rootElement.insertAdjacentHTML('beforeend', showAddSongIfNone(result));
+}
+
+function showAddSongIfNone(genres) {
+    return `
+        <div id="addSongBox">
+            <h1>Your playlist is currently empty!</h1>
+            <h3>Add some songs to it.</h3>
+            <div id="myForm"> 
+                <label for="artist">Artist </label>
+                <input class="input" id="artist" type="text" placeholder="Artist">
+                <br>
+                <label for="title">Title </label>
+                <input class="input" id="title" type="text" placeholder="Title">
+                <br>
+                <label for="file">File </label>
+                <input name="fuResume" type="file" class="input" id="fuResume" accept="audio/mp3, audio/wav">
+                <br>
+                <label for="genre">Genre </label>
+                <select class="input" id="genre"  placeholder="Select Genre">
+                    <option value="default">None</option>
+                   ${genres.map(x => showOptions(x))}
+                </select>
+                <br>
+                <button class="input" type="submit" id="submit">Add File</button>
+            </div>
+        </div>
+    `
+}
+
+function showOptions(genres) {
+    return `
+        <option id="${genres.id}" value="${genres.genre}">${genres.genre}</option>
+    `
+}
+
+window.addEventListener('load', initRender);
