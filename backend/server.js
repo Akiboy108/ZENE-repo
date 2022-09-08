83fdd6ba47d4781efe2960d08ed463230ea29cb1
Akos <barcza.akos15@gmail.com>
2022-09-08 22:26:27 +0200

    modify api/add endpoint post request -> can now create folders and copy files in it

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import fs from 'fs';
 import path from 'path';
 
-const __dirname = 'D:/WEB/htdocs/Webfejlesztés/VS_Code/ZENE-repo/frontend/USERS'
+const __dirname = 'D:/WEB/htdocs/Webfejlesztés/VS_Code/ZENE-repo/frontend/user';
+const __index_html = 'D:/WEB/htdocs/Webfejlesztés/VS_Code/ZENE-repo/frontend/copyFiles/index.html';
+const __style_css = 'D:/WEB/htdocs/Webfejlesztés/VS_Code/ZENE-repo/frontend/copyFiles/style.css';
+const __script_js = 'D:/WEB/htdocs/Webfejlesztés/VS_Code/ZENE-repo/frontend/copyFiles/script.js';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -14,7 +17,7 @@ app.get('/api/genre', (req, res) => {
     res.send(genres);
 });
 
-app.get('/api/music', (req, res) => {
+app.get('/api/music', (req, res) => {//Shows raw json data
     const data = JSON.parse(fs.readFileSync('./backend/playlist.json', 'utf8'));
     res.send(data);
 });
@@ -29,12 +32,12 @@ app.get('/api/getSongs/:name', (req, res) => {//CHECKS IF CLICKED PL IS EMPTY
     }
 });
 
-app.get('/api/mkdir/:id', (req, res) => {
+/* app.get('/api/mkdir/:id', (req, res) => {
     const data = JSON.parse(fs.readFileSync('./backend/playlist.json', 'utf8'));
     const choosenList = data.playlist.find(x => x.id === parseInt(req.params.id));
-    fs.access(`${__dirname}/${choosenList.name}`, (error) => {
+    fs.access(`${__dirname}/${choosenList.name}`, (error) => { //Checks if given directory already exists
         if (error) {
-            fs.mkdir(path.join(__dirname, `${choosenList.name.split(' ').join('_')}`), (err) => {
+            fs.mkdir(path.join(__dirname, `${choosenList.name.split(' ').join('_')}`), (err) => {//Creates new direcotry
                 if (err) {
                     return console.warn(err)
                 }
@@ -46,6 +49,7 @@ app.get('/api/mkdir/:id', (req, res) => {
         }
     })
 })
+ */
 
 app.get('/api/personalplaylist', (req, res) => {//SEND DATA ABOUT ALL PL
     const data = JSON.parse(fs.readFileSync('./backend/playlist.json', 'utf8'));
@@ -86,8 +90,47 @@ app.post('/api/add/:name', (req, res) => { //CREATE NEW PLAYLIST
     const newPlaylist = { id: newID, name: req.params.name, [req.params.name]: body.playlist, password: body.pw/* list: body.playlist */ }
     const newArtist = { name: body.artist }
     data.playlist.push(newPlaylist);
+    const choosenList = data.playlist.find(x => x.id === newPlaylist.id);
     /* data.artists.push(newArtist); */
     fs.writeFileSync('./backend/playlist.json', JSON.stringify(data, undefined, 4), 'utf8');
+    fs.access(`${__dirname}/${choosenList.name}`, (error) => { //Checks if given directory already exists
+        if (error) {
+            fs.mkdir(path.join(__dirname, `${choosenList.name.split(' ').join('_')}`), (err) => {//Creates new direcotry
+                if (err) {
+                    console.log('* * * Folder cannot be created!')
+                }
+                console.log("* * * Folder successfully created...")
+            });
+            fs.copyFile(__index_html, `${__dirname}/${choosenList.name.split(' ').join('_')}/index.html`, (err) => {
+                if (err) {
+                    console.log(`* * * Cannot copy HTML file to this location! ${err}`);
+                }
+                else {
+                    console.log('* * * HTML file succesfully copied to the new destination!');
+                }
+            });
+            fs.copyFile(__script_js, `${__dirname}/${choosenList.name.split(' ').join('_')}/script.js`, (err) => {
+                if (err) {
+                    console.log(`* * * Cannot copy JS file to this location! ${err}`);
+                }
+                else {
+                    console.log('* * * JS file succesfully copied to the new destination!');
+                }
+            });
+            fs.copyFile(__style_css, `${__dirname}/${choosenList.name.split(' ').join('_')}/style.css`, (err) => {
+                if (err) {
+                    console.log(`* * * Cannot copy CSS file to this location! ${err}`);
+                }
+                else {
+                    console.log('* * * CSS file succesfully copied to the new destination!');
+                }
+            });
+        }
+        else {
+            console.log('* * * Given directory already exists!')
+        }
+    })
+
     //fs.writeFileSync('./backend/artists.json', JSON.stringify(data, undefined, 4), 'utf8');
     res.send(newPlaylist);
 });
@@ -98,8 +141,12 @@ app.delete('/api/delete/:id', (req, res) => {//DELETE THE CORECT PL
     const index = data.playlist.indexOf(removable);
     data.playlist.splice(index, 1);
     fs.writeFileSync('./backend/playlist.json', JSON.stringify(data, undefined, 4), 'utf8');
-    console.log('data::: ', data)
-    console.log('play::: ', data.playlist)
+    fs.rm(`${__dirname}/${removable.name}`, { recursive: true }, err => {//delete directory of playlist
+        if (err) {
+            console.log('* * * Something went wrong! Directory cannort be removed!')
+        }
+        console.log(`* * * '${removable.name}' directory is deleted succesfully!`)
+    })
     res.send(data.playlist)
 });
 
