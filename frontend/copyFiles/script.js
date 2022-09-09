4da578747b54b7f846281601efc3fa9de3f0c5d7
Akos <barcza.akos15@gmail.com>
2022-09-09 13:42:38 +0200

    add form

--- frontend/copyFiles/script.js
+++ frontend/copyFiles/script.js
@@ -44,22 +44,24 @@ function showAddSongIfNone(genres) {
             <h1>Your playlist is currently empty!</h1>
             <h3>Add some songs to it.</h3>
             <div id="myForm"> 
-                <label for="artist">Artist </label>
-                <input class="input" id="artist" type="text" placeholder="Artist">
-                <br>
-                <label for="title">Title </label>
-                <input class="input" id="title" type="text" placeholder="Title">
-                <br>
-                <label for="file">File </label>
-                <input name="fuResume" type="file" class="input" id="fuResume" accept="audio/mp3, audio/wav">
-                <br>
-                <label for="genre">Genre </label>
-                <select class="input" id="genre"  placeholder="Select Genre">
-                    <option value="default">None</option>
-                   ${genres.map(x => showOptions(x))}
-                </select>
-                <br>
-                <button class="input" type="submit" id="submit">Add File</button>
+                <form id="form">
+                    <label for="artist">Artist </label>
+                    <input class="input" id="artist" type="text" placeholder="Artist">
+                    <br>
+                    <label for="title">Title </label>
+                    <input class="input" id="title" type="text" placeholder="Title">
+                    <br>
+                    <label for="file">File </label>
+                    <input name="fuResume" type="file" class="input" id="fuResume" accept="audio/mp3, audio/wav">
+                    <br>
+                    <label for="genre">Genre </label>
+                    <select class="input" id="genre"  placeholder="Select Genre">
+                        <option value="default">None</option>
+                       ${genres.map(x => showOptions(x))}
+                    </select>
+                    <br>
+                    <button class="input" type="submit" id="submit">Add File</button>
+                </form>
             </div>
         </div>
     `
